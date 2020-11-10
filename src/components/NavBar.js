849115121ca06ab76b6d7f9c86328b8f62c4dee2
MeerHamza1421 <hamzababar37@gmail.com>
2020-11-10 12:41:06 +0500

    Code of title Bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import AppIcon from './images/app-icon.jpg';
 
 const NavBar = () => (
-    <>
-        <nav>Meme Generator</nav>
-    </>
+    <nav>
+        <img src={AppIcon} alt="something went wrong" />
+        <h3>Meme Generator</h3>
+    </nav>
 );
 
 export default NavBar
