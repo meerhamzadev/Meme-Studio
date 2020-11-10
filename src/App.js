8a4d00ec1552fa243f5b0b90069194825e71f94d
MeerHamza1421 <hamzababar37@gmail.com>
2020-11-10 12:48:38 +0500

    Main Working area of our app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from "./components/NavBar";
 import './App.css';
+import WelcomePage from './components/WelcomePage';
 function App() {
   return (
     <>
       <NavBar />
+      <WelcomePage />
     </>
   );
 }
