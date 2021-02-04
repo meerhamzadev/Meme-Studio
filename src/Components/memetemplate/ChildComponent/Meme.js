d1b070cc967c92168796289ff1e923804015ee5e
MeerHamza1421 <hamzababar37@gmail.com>
2021-02-05 00:15:27 +0500

    Make meme templates formatting section functional

--- src/Components/memetemplate/ChildComponent/Meme.js
+++ src/Components/memetemplate/ChildComponent/Meme.js
@@ -4,14 +4,14 @@ const Meme = props => {
     const [textColor, setTextColor] = useState('#000000')
     return (
         <section className="MemeContainer">
+
             <div id="Meme" className="MemeContainer__Meme"
                 style={{ backgroundImage: `url(${props.meme})` }}>
-
                 <h2 className="MemeContainer__Text" style={{ color: `${textColor}`, fontSize: `${props.textSize}px` }} >{props.topText}</h2>
                 <h2 className="MemeContainer__Text" style={{ color: `${textColor}`, fontSize: `${props.textSize}px` }} >{props.bottomText}</h2>
-
             </div>
-            <SketchPicker color={textColor}
+
+            <SketchPicker color={textColor} className="MemeContainer__ColorPicker"
                 onChange={updateColor => setTextColor(updateColor.hex)} />
         </section>
     );
