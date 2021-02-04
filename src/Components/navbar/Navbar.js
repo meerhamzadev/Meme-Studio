5047d55d5f5cd26688340f29f90fc15897666670
MeerHamza1421 <hamzababar37@gmail.com>
2021-02-05 00:16:15 +0500

    Update navbar

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,22 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Logo from '../assets/logo.png';
 
 function Navbar() {
+
     return (
         <nav className="Navbar">
-            <div className="Navbar__logo">
+            <Link to="/" className="Navbar__logo">
                 <img src={Logo} alt="Logo" className="Navbar__logo__image" />
-            </div>
+            </Link>
             <div className="Navbar__Info">
                 <h1 className="Navbar__Info__Title">Meme Gallery</h1>
-                <form className="Navbar__Info__Search">
-                    <input type="text" placeholder="Enter the template name" className="Navbar__Info__Search__Box" />
-                    <button className="Navbar__Info__Search__Button">🔎</button>
-                </form>
             </div>
-            {/* <h1 className="Navbar__Title">
-                Meme Gallery
-            </h1> */}
         </nav>
     )
 }
